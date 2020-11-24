d3a799e4de11c7dd3aeec6584d1f7d0d9cc5757c
RedAlex <RedyAlex@bitbucket.org>
2020-11-24 17:33:48 +0300

    'start'

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const {src, dest, parallel, series, watch} = require('gulp')
+const concat = require('gulp-concat')
+const minify = require("gulp-babel-minify")
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const imagemin = require('gulp-imagemin')
+
+function js(){
+    return src('assets/js/script.js')
+        .pipe(concat('script.min.js'))
+        .pipe(minify({
+            mangle: {
+                keepClassName: true
+            }
+    }))
+    .pipe(dest('src/js/'))
+}
+
+function styles() {
+    return src('assets/css/style.scss')
+    .pipe(sass())
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+        grid: true
+    }))
+    .pipe(cleanCSS())
+    .pipe(dest('src/css/'))
+}
+
+function fonts() {
+    return src('assets/fonts/**/*')
+    .pipe(dest('src/fonts/'))
+}
+
+function gwatch() {
+    watch('assets/js/*.js', js)
+    watch('assets/css/*.scss', styles)
+    watch('assets/fonts/**/*', fonts)
+    watch('assets/img/**/*', img)
+}
+
+function img() {
+    return src('assets/img/**/*')
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 5})
+        ]))
+        .pipe(dest('src/img/'))
+}
+
+exports.js = js
+exports.css = styles
+exports.fonts = fonts
+exports.default = series(parallel(js, styles, gwatch))
---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/style.min.css">
+    <title>Тестовое задание</title>
+</head>
+<body>
+    <script src="src/js/script.min.js"></script>
+</body>
+</html>
